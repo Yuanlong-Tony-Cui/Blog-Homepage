a8d48a9d729b97fc87fcbc3b2cd4e9eb941fe4f8
Yuanlong-Tony-Cui <yuanlongcui0511@gmail.com>
2020-10-15 13:07:35 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,35 +6,40 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Young as the Morning, Old as the Sea
-        </p>
+        <div>
+          <p>
+            巴颜
+          </p>
+          <p style={{font:"monospace", fontSize:"20px"}}>
+            Young as the Morning, Old as the Sea
+          </p>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" style={{marginTop:"50px", marginBottom:"50px"}}/>
         <div className="List">
-        <a
-          className="App-link"
-          href="https://yuanlong-tony-cui.github.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Blog
-        </a>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/yuanlong-tony-cui/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/Yuanlong-Tony-Cui"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
+          <a
+            className="App-link"
+            href="https://yuanlong-tony-cui.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Blog
+          </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/yuanlong-tony-cui/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/Yuanlong-Tony-Cui"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
         </div>
       </header>
     </div>
