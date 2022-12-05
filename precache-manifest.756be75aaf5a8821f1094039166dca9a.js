8c9f48a44c897ca9180cb6a48b2a98e12b20ebea
Yuanlong-Tony-Cui <yuanlongcui0511@gmail.com>
2022-12-04 22:00:06 -0500

    Updates

---
+++ precache-manifest.756be75aaf5a8821f1094039166dca9a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475144d5295e1e9bea82f2ae036b75b0",
+    "url": "/Blog-Homepage/index.html"
+  },
+  {
+    "revision": "bd13cdb9fdad42d35849",
+    "url": "/Blog-Homepage/static/css/main.1262feb9.chunk.css"
+  },
+  {
+    "revision": "ef4fa383d669fd91c7bc",
+    "url": "/Blog-Homepage/static/js/2.2c036b3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Blog-Homepage/static/js/2.2c036b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd13cdb9fdad42d35849",
+    "url": "/Blog-Homepage/static/js/main.2f6a2e08.chunk.js"
+  },
+  {
+    "revision": "391daebcf2ad2453550c",
+    "url": "/Blog-Homepage/static/js/runtime-main.9acad823.js"
+  },
+  {
+    "revision": "7b18ebb26fd5922a9869529f86518e0d",
+    "url": "/Blog-Homepage/static/media/allan.7b18ebb2.jfif"
+  },
+  {
+    "revision": "fe199288649d4d2b69561d14d608868a",
+    "url": "/Blog-Homepage/static/media/gen3-robot.fe199288.webp"
+  },
+  {
+    "revision": "071628854d38d5a0b9df634da7b72599",
+    "url": "/Blog-Homepage/static/media/mountains_and_seas.07162885.jpg"
+  },
+  {
+    "revision": "ec78bca476c8417235dc222acf87cc3b",
+    "url": "/Blog-Homepage/static/media/ryan.ec78bca4.jfif"
+  },
+  {
+    "revision": "7fbc3449feacb1c34585bf971e30858d",
+    "url": "/Blog-Homepage/static/media/tony.7fbc3449.jpg"
+  }
+]);
